ff1ea506d171f9600ea4486c12e01fcbd0a3a1e2
RNO <arnaud.juracek@gmail.com>
2017-10-17 12:43:29 +0200

    Handle stdin key event in dev env
    The viewer process is now spawned in detached mode to allow killing it more reliably

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ process.title = 'stratum'
 const path = require('path')
 const { paths, env } = require(path.join(__dirname, 'main.config.js'))
 
-if (env === 'development') {
-  require(path.join(paths.utils, 'dev-helper'))({
-    // @TODO: supply stratum-viewer binaries
-    viewer: '/Users/RNO/Projects/chevalvert/stratum/stratum-viewer/sketch'
-  })
-}
-
 const stratum = require(path.join(paths.lib, 'stratum'))
 const animations = require(path.join(paths.lib, 'animations-manager'))([
   require(path.join(paths.animations, 'debug')),
@@ -21,10 +14,22 @@ const animations = require(path.join(paths.lib, 'animations-manager'))([
 ])
 
 stratum.add(animations.update)
-animations.select('earth')
-
 stratum.start()
-
 stratum.server.on('newnode', () => {
   !animations.running && animations.resume()
 })
+
+animations.select('rain')
+
+// -------------------------------------------------------------------------
+
+if (env === 'development') {
+  require(path.join(paths.utils, 'dev-helper'))({
+    // @TODO: supply stratum-viewer binaries
+    viewer: '/Users/RNO/Projects/chevalvert/stratum/stratum-viewer/sketch',
+    onKeyPress: (str, key) => {
+      if (key.name === 'left') return animations.previous()
+      if (key.name === 'right') return animations.next()
+    }
+  })
+}

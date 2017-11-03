277c3d0a006d942edd77a0117acad3160facafd1
RNO <arnaud.juracek@gmail.com>
2017-11-03 11:28:20 +0100

    [debug] Add leap input

--- animations/debug.js
+++ animations/debug.js
@@ -9,25 +9,18 @@ const { hand }  = require(path.join(paths.lib, 'leap'))
 module.exports = class Debug extends Animation {
   constructor (manager, offset) {
     super(manager, offset)
-    this.color = [
-      Math.random() * 255,
-      Math.random() * 255,
-      Math.random() * 255
-    ]
   }
 
   update (dt) {
     super.update(dt)
-
     this.clear()
 
-    let pos = hand([this.width, this.depth, this.height])
-    if (pos) {
-      for (let x = 0; x < this.width; x++) {
-        for (let y = 0; y < this.depth; y++) {
-          for (let z = 0; z < pos.z; z++) {
-            this.set(x, y, z)
-          }
+    let pos = hand([this.width, this.depth, this.height]) || { x: this.width , y: this.depth, z: this.height / 2 }
+
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.depth; y++) {
+        for (let z = 0; z < pos.z; z++) {
+          this.set(x, y, z, [config.white[0], config.white[1], config.white[2]])
         }
       }
     }

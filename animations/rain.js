1ab07de7f8d1aea447cf5e6c75b63345d2f2995a
RNO <arnaud.juracek@gmail.com>
2017-11-03 11:47:56 +0100

    [rain] Add leap input + Fix trail drawing

--- animations/rain.js
+++ animations/rain.js
@@ -4,6 +4,7 @@ const path = require('path')
 const { paths, config } = require(path.join(__dirname, '..', 'main.config.js'))
 const { Vec3 } = require('vec23')
 const { map } = require('missing-math')
+const { hand }  = require(path.join(paths.lib, 'leap'))
 
 const Particle = require(path.join(paths.utils, 'particle'))
 const Animation = require(path.join(paths.utils, 'animation'))
@@ -12,7 +13,7 @@ module.exports = class Rain extends Animation {
   constructor (manager, offset) {
     super(manager, offset)
     this.particles = []
-    for (let i = 0; i < config.animations.rain.particlesLength; i++) {
+    for (let i = 0; i < this.config.particlesLength[0]; i++) {
       this.particles[i] = this.createRandomParticle()
     }
   }
@@ -21,13 +22,34 @@ module.exports = class Rain extends Animation {
     super.update(dt)
     this.clear()
 
+    const h = hand([1, 1, 1]) || { x: 0.5, y: 0.5, z: 0.5 }
+    const particlesLength = map(h.x, 0, 1, this.config.particlesLength[0], this.config.particlesLength[1])
+    const maxSpeed = map(h.y, 0, 1, this.config.particle.maxSpeed[0], this.config.particle.maxSpeed[1])
+    const trailLength = map(h.z, 0, 1, this.config.particle.trailLength[0], this.config.particle.trailLength[1])
+
+    const particleOpts = {
+      ...this.config.particle,
+      maxSpeed,
+      trailLength: trailLength | 0
+    }
+
+    if (this.particles.length < particlesLength) {
+      for (let i = 0; i < particlesLength - this.particles.length; i++) {
+        this.particles.push(this.createRandomParticle(particleOpts))
+      }
+    }
+
     this.particles.forEach((particle, index) => {
+      particle.opts.maxSpeed = maxSpeed
+      particle.opts.trailLength = trailLength
       particle.update()
 
       if (particle.oob && particle.z < 0) {
-        this.drawRipple(particle)
+        if (this.config.ripples) this.drawRipple(particle)
         if (Particle.isOob(particle.lastTrail)) {
-          this.particles[index] = this.createRandomParticle()
+          if (index < particlesLength) {
+            this.particles[index] = this.createRandomParticle(particleOpts)
+          } else this.particles.splice(index, 1)
         }
       }
 
@@ -35,11 +57,11 @@ module.exports = class Rain extends Animation {
     })
   }
 
-  createRandomParticle () {
+  createRandomParticle (particleOpts) {
     let x = Math.random() * this.width
     let y = Math.random() * this.depth
     let z = this.height + Math.random() * this.height
-    return new Particle(x, y, z, config.animations.rain.particle)
+    return new Particle(x, y, z, particleOpts)
   }
 
   drawRipple (particle) {
@@ -55,13 +77,16 @@ module.exports = class Rain extends Animation {
   }
 
   drawTrail (particle) {
-    particle.trail.forEach((point, index) => {
-      let v = (index / (particle.trail.length - 1))
+    let za = particle.z
+    let zb = particle.lastTrail.z
+
+    for (let z = za; z < zb; z++) {
+      let v = map(z, za, zb, 1, 0) ** 2
       let r = v * config.white[0]
       let g = v * config.white[1]
       let b = v * config.white[2]
-      this.set(point.x, point.y, point.z, [r, g, b])
-    })
+      this.set(particle.x, particle.y, z, [r, g, b])
+    }
   }
 
 }

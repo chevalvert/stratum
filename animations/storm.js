86cf74830eb658814d46b2160af86842ad4d9484
RNO <arnaud.juracek@gmail.com>
2017-11-15 12:31:59 +0100

    [storm] Move strike into its own component + add leap input

--- animations/storm.js
+++ animations/storm.js
@@ -3,7 +3,8 @@
 const path = require('path')
 const { paths, config } = require(path.join(__dirname, '..', 'main.config.js'))
 const Animation = require(path.join(paths.utils, 'animation'))
-const { lerp, clamp } = require('missing-math')
+const Strike = require(path.join(paths.utils, 'strike'))
+const { map } = require('missing-math')
 const { hand }  = require(path.join(paths.lib, 'leap'))
 
 module.exports = class Storm extends Animation {
@@ -17,53 +18,32 @@ module.exports = class Storm extends Animation {
     super.update(dt)
     this.clear()
 
-    const delay = lerp(this.config.delay[0], this.config.delay[1], Math.random())
-    if (this.count > this.lastStrike + delay && this.strikes.length < this.config.maxStrikesLength) {
-      const lifetime = lerp(this.config.duration[0], this.config.duration[1], Math.random())
-      this.spawn({lifetime})
-    }
+    const h = hand()
+    const delay = map(h.y, 0, 1, this.config.delay[0], this.config.delay[1])
+    const lifetime = map(h.z, 0, 1, this.config.duration[0], this.config.duration[1])
+    const color = this.config.colors[Math.floor(h.x * (this.config.colors.length - 1))]
+
+    this.canSpawn(delay) && this.spawn({ lifetime, color })
 
     this.strikes.forEach(strike => {
-      this.move(strike)
-      this.draw(strike)
-      if (this.count > strike.end) this.remove(strike)
-    })
-  }
-
-  spawn ({x, y, lifetime}) {
-    this.lastStrike = this.count
-    this.strikes.push({
-      x: x || Math.floor(Math.random() * this.width - 1),
-      y: y || Math.floor(Math.random() * this.depth - 1),
-      start: this.count,
-      end: this.count + lifetime,
-      target: {
-        x: Math.floor(Math.random() * this.width - 1),
-        y: Math.floor(Math.random() * this.depth - 1)
+      if (strike) {
+        strike.update()
+        strike.draw()
+        strike.isDead && this.remove(strike)
       }
     })
   }
 
-  move (strike) {
-    const roff = Math.random() >= 0.5 ? -1 : 1
-    const dirX = Math.sign(strike.target.x - strike.x) || roff
-    const dirY = Math.sign(strike.target.y - strike.y) || roff
-
-    if (Math.random() >= 0.5) strike.x = strike.x + dirX
-    else strike.y = strike.y + dirY
-
-    if (strike.x === strike.target.x && strike.y === strike.target.y) {
-      strike.target = {
-        x: Math.floor(Math.random() * this.width - 1),
-        y: Math.floor(Math.random() * this.depth - 1)
-      }
-    }
+  canSpawn (delay) {
+    return this.count > this.lastStrike + delay
+        && this.strikes.length < this.config.maxStrikesLength
   }
 
-  draw (strike) {
-    for (let z = 0; z < this.height; z++) {
-      this.set(strike.x, strike.y, z, this.config.color)
-    }
+  spawn (opts) {
+    const strike = new Strike(this, opts)
+
+    this.strikes.push(strike)
+    this.lastStrike = strike.start
   }
 
   remove (strike) {
